057746b71ba725aa0b36a82c6eb6d5d9194b9452
dosun <lds3794@gmail.com>
2015-08-06 01:04:22 +0900

    Create Project Test
    Input :
    DXUT.lib
    DXUTOpt.lib
    d3dx9d.lib
    dxerr.lib
    dxguid.lib
    winmm.lib
    comctl32.lib

---
+++ source/Test/Main.cpp
@@ -0,0 +1,126 @@
+//--------------------------------------------------------------------------------------
+// File: framework.cpp
+//
+// Empty starting point for new Direct3D 9 and/or Direct3D 10 applications
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//--------------------------------------------------------------------------------------
+#include "DXUT.h"
+#include "Globals.h"
+
+
+//--------------------------------------------------------------------------------------
+// Extern declarations 
+//--------------------------------------------------------------------------------------
+extern bool CALLBACK IsD3D9DeviceAcceptable(D3DCAPS9* pCaps, D3DFORMAT AdapterFormat, D3DFORMAT BackBufferFormat,
+	bool bWindowed, void* pUserContext);
+extern HRESULT CALLBACK OnD3D9CreateDevice(IDirect3DDevice9* pd3dDevice,
+	const D3DSURFACE_DESC* pBackBufferSurfaceDesc, void* pUserContext);
+extern HRESULT CALLBACK OnD3D9ResetDevice(IDirect3DDevice9* pd3dDevice, const D3DSURFACE_DESC* pBackBufferSurfaceDesc,
+	void* pUserContext);
+extern void CALLBACK OnD3D9FrameRender(IDirect3DDevice9* pd3dDevice, double fTime, float fElapsedTime,
+	void* pUserContext);
+extern void CALLBACK OnD3D9LostDevice(void* pUserContext);
+extern void CALLBACK OnD3D9DestroyDevice(void* pUserContext);
+
+//--------------------------------------------------------------------------------------
+// Called right before creating a D3D9 or D3D10 device, allowing the app to modify the device settings as needed
+//--------------------------------------------------------------------------------------
+bool CALLBACK ModifyDeviceSettings(DXUTDeviceSettings* pDeviceSettings, void* pUserContext)
+{
+	return true;
+}
+
+//--------------------------------------------------------------------------------------
+// Handle updates to the scene.  This is called regardless of which D3D API is used
+//--------------------------------------------------------------------------------------
+void CALLBACK OnFrameMove(double fTime, float fElapsedTime, void* pUserContext)
+{
+}
+
+//--------------------------------------------------------------------------------------
+// Handle messages to the application
+//--------------------------------------------------------------------------------------
+LRESULT CALLBACK MsgProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam,
+	bool* pbNoFurtherProcessing, void* pUserContext)
+{
+	return 0;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Handle key presses
+//--------------------------------------------------------------------------------------
+void CALLBACK OnKeyboard(UINT nChar, bool bKeyDown, bool bAltDown, void* pUserContext)
+{
+}
+
+
+//--------------------------------------------------------------------------------------
+// Handle mouse button presses
+//--------------------------------------------------------------------------------------
+void CALLBACK OnMouse(bool bLeftButtonDown, bool bRightButtonDown, bool bMiddleButtonDown,
+	bool bSideButton1Down, bool bSideButton2Down, int nMouseWheelDelta,
+	int xPos, int yPos, void* pUserContext)
+{
+}
+
+
+//--------------------------------------------------------------------------------------
+// Call if device was removed.  Return true to find a new device, false to quit
+//--------------------------------------------------------------------------------------
+bool CALLBACK OnDeviceRemoved(void* pUserContext)
+{
+	return true;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Initialize everything and go into a render loop
+//--------------------------------------------------------------------------------------
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+	// Enable run-time memory check for debug builds.
+#if defined(DEBUG) | defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	// DXUT will create and use the best device (either D3D9 or D3D10) 
+	// that is available on the system depending on which D3D callbacks are set below
+	
+	// Set general DXUT callbacks
+	DXUTSetCallbackFrameMove(OnFrameMove);
+	DXUTSetCallbackKeyboard(OnKeyboard);
+	DXUTSetCallbackMouse(OnMouse);
+	DXUTSetCallbackMsgProc(MsgProc);
+	DXUTSetCallbackDeviceChanging(ModifyDeviceSettings);
+	DXUTSetCallbackDeviceRemoved(OnDeviceRemoved);
+
+	// Set the D3D9 DXUT callbacks. Remove these sets if the app doesn't need to support D3D9
+	DXUTSetCallbackD3D9DeviceAcceptable(IsD3D9DeviceAcceptable);
+	DXUTSetCallbackD3D9DeviceCreated(OnD3D9CreateDevice);
+	DXUTSetCallbackD3D9DeviceReset(OnD3D9ResetDevice);
+	DXUTSetCallbackD3D9FrameRender(OnD3D9FrameRender);
+	DXUTSetCallbackD3D9DeviceLost(OnD3D9LostDevice);
+	DXUTSetCallbackD3D9DeviceDestroyed(OnD3D9DestroyDevice);
+
+	// Perform any application-level initialization here
+
+	// Setup Video
+	VideoSetup setup(1024, 720);
+	setup.isWindowed = true;
+
+	// Rename Game Title
+	g_strGameTitle = L"MyGame";
+
+	DXUTInit(true, true, NULL); // Parse the command line, show msgboxes on error, no extra command line params
+	DXUTSetCursorSettings(true, true); // Show the cursor and clip it when in full screen
+	//HICON hIcon = (HICON)LoadImage(NULL, L"..\..\resources\WindowIcon.ico", IMAGE_ICON, 32, 32, LR_LOADFROMFILE);
+	DXUTCreateWindow(g_strGameTitle);
+	DXUTCreateDevice(setup.isWindowed, setup.height, setup.witdh);
+	DXUTMainLoop(); // Enter into the DXUT render loop
+
+	// Perform any application-level cleanup here
+
+	return DXUTGetExitCode();
+}
